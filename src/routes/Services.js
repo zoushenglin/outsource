1eee6c7eb0dd1a938bc870bc1c56a855ef67040f
zoushenglin <512055076@qq.com>
2017-09-15 09:27:26 +0800

    Delete Services.js

--- src/routes/Services.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Banner from '../components/Banner';
-export default class Services extends React.Component{
-    render = ()=>
-    <div>
-        <Banner />
-    </div>
-}
